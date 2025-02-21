97fd10c79e685e82ac7bd79a61d289afea1d66c0
NushratNowrin <nowrin2023@gmail.com>
2025-02-21 20:52:47 +0600

    sun moon loading done

--- JS/style-switcher.js
+++ JS/style-switcher.js
@@ -21,3 +21,13 @@ function setActiveStyle(color) {
 		}
 	});
 }
+
+// Theme light and dark mode
+const dayNight = document.querySelector(".day-night");
+window.addEventListener("load", () => {
+	if (document.body.classList.contains("dark")) {
+		dayNight.querySelector("i").classList.add("fa-sun");
+	} else {
+		dayNight.querySelector("i").classList.add("fa-moon");
+	}
+});
